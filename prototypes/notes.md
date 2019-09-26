c0d29e20fa7351bf64df7c25bd46b0ef240c01af
Emily Horsman <me@emilyhorsman.com>
2019-09-25 23:23:52 -0400

    Prototyping with LIS3D4 to determine client config needs

---
+++ prototypes/notes.md
@@ -0,0 +1,6 @@
+# LIS3DH
+
+- `STATUS_REG` before checking data (proven unnecessary in the end)
+- Setting accelerometer range (https://github.com/adafruit/Adafruit_LIS3DH/blob/master/examples/acceldemo/acceldemo.ino#L36)
+- Autoincrement support but as far as processor is concerned this is simply a different register/num bytes
+- Number of things to write on setup
--- prototypes/src/main.cpp
+++ prototypes/src/main.cpp
@@ -11,54 +11,82 @@ void setup()
 
     wire->begin();
     wire->setTimeout(5);
+    wire->beginTransmission(0x18);
+    wire->write(0x20);
+    wire->write(0b01110111);
+    wire->endTransmission();
     delay(1000);
 }
 
 void loop()
 {
-    /*
-        SHT31-D
+    /*char buff[6];
+    wire->beginTransmission(0x44);
+    wire->write(0x24);
+    wire->write(0x0);
+    wire->endTransmission();
+    delay(30);
+    wire->requestFrom(0x44, 6);
+    wire->readBytes(buff, 6);
 
-        char buff[6];
-        wire->beginTransmission(0x44);
-        wire->write(0x24);
-        wire->write(0x0);
-        wire->endTransmission();
-        delay(30);
-        wire->requestFrom(0x44, 6);
-        wire->readBytes(buff, 6);
+    uint16_t temp;
+    temp = buff[0];
+    temp <<= 8;
+    temp |= buff[1];
 
-        uint16_t temp;
-        temp = buff[0];
-        temp <<= 8;
-        temp |= buff[1];
+    double t = temp;
+    t *= 175;
+    t /= 0xffff;
+    t = -45 + t;
+    Serial.printf("Temp: %f\n", t);*/
 
-        double t = temp;
-        t *= 175;
-        t /= 0xffff;
-        t = -45 + t;
-        Serial.printf("Temp: %f\n", t);
-    */
 
-    /*
+
+    wire->beginTransmission(0x18);
+    // Set the MSB in the 8 bit address to 1 so that it will auto increment.
+    // Page 21/42 of https://cdn-shop.adafruit.com/datasheets/LIS3DH.pdf
+    wire->write(0x28 | 128); 
+    wire->endTransmission();
+    delay(20);
+    uint8_t buffb[6];
+    wire->requestFrom(0x18, 6);
+    uint16_t x, y, z;
+    float xg, yg, zg;
+    if (wire->readBytes(buffb, 6) == 6) {
+        uint16_t x = buffb[0];
+        x |= buffb[1] << 8;
+        uint16_t y = buffb[2];
+        y |= buffb[3] << 8;
+        uint16_t z = buffb[4];
+        z |= buffb[5] << 8;
+        xg = (float) x / 16380;
+        yg = (float) y / 16380;
+        zg = (float) z / 16380;
+        Serial.printf("[%d] 0x18 %f %f %f\n", count++, xg, yg, zg);
+    }
+
+    // Test multiple register reads, should match values.
+    for (int i = 0; i < 6; i++) {
         wire->beginTransmission(0x18);
-        wire->write(0x0F);
+        wire->write(0x28 + i);
         wire->endTransmission();
         delay(20);
-
-        uint8_t buff[1];
         wire->requestFrom(0x18, 1);
-        if (wire->readBytes(buff, 1) == 1) {
-        Serial.printf("[%d] 0x18 ID: %x\n", count++, *buff);}
-    */
+        buffb[i] = wire->read();
+    }
+    x = buffb[0];
+    x |= buffb[1] << 8;
+    y = buffb[2];
+    y |= buffb[3] << 8;
+    z = buffb[4];
+    z |= buffb[5] << 8;
+    xg = (float) x / 16380;
+    yg = (float) y / 16380;
+    zg = (float) z / 16380;
+    Serial.printf("[%d] 0x18 %f %f %f\n", count, xg, yg, zg);
 
-    // wire->beginTransmission(0x44);
-    //wire->write(0x0);
-    // wire->endTransmission();
-    delay(20);
 
-    char buff[1];
-
+    /*
     for (uint8_t i=0; i < 128; i++)
     {
         wire->requestFrom(i, 1);
@@ -66,6 +94,6 @@ void loop()
             Serial.printf("Found %x\n", i);
         else
             Serial.printf("Nothing on %x\n", i);
-    }
-    delay(10 * 1000);
+    }*/
+    delay(1000);
 }
