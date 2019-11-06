e53e77538435eb783398b264f3afa9f7d65a57db
Emily Horsman <me@emilyhorsman.com>
2019-11-05 21:49:43 -0500

    Start proof of concept for I2C runtime

---
+++ prototypes/src/I2CRuntime.cpp
@@ -0,0 +1,49 @@
+#include "Arduino.h"
+
+#define REGISTER_REQ_DELAY_MILLI 20
+
+struct OutputRead {
+    uint16_t outputId;
+    uint16_t registerId;
+    // The "no need" behaviour here is a value of 1.
+    uint16_t manualAdvanceNum;
+    uint8_t numBytesPerAdvance;
+    uint16_t pollIntervalMilli;
+};
+
+typedef struct OutputRead OutputRead;
+
+struct Peripheral {
+    uint16_t busAddr;
+    uint8_t numOutputs;
+    OutputRead * outputs;
+};
+
+typedef struct Peripheral Peripheral;
+
+// Not the way we actually want to do it, this is blocking non-loop code
+// Just a proof of concept for the runtime struct
+void prototypeOutputRead(
+    TwoWire *wire,
+    uint16_t busAddr,
+    OutputRead *conf
+) {
+    uint8_t *bytes = new uint8_t[conf->numBytesPerAdvance * conf->manualAdvanceNum];
+    for (uint16_t i = 0; i < conf->manualAdvanceNum; i++) {
+        wire->beginTransmission(busAddr);
+        wire->write(conf->registerId + i);
+        wire->endTransmission();
+        delay(REGISTER_REQ_DELAY_MILLI);
+        wire->requestFrom(busAddr, conf->numBytesPerAdvance);
+
+        for (uint8_t j = 0; j < conf->numBytesPerAdvance; j++) {
+            bytes[i * conf->numBytesPerAdvance + j] = wire->read();
+        }
+    }
+}
+
+void prototype(TwoWire *wire, Peripheral *peripheral) {
+    for (uint8_t i = 0; i < peripheral->numOutputs; i++) {
+        prototypeOutputRead(wire, peripheral->busAddr, peripheral->outputs[i]);
+    }
+}
