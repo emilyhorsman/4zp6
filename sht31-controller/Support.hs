a060b0081bc22d9ed4124a3226663fc830c8b134
Emily Horsman <me@emilyhorsman.com>
2020-04-29 01:42:36 -0400

    Expand interface for data subscription

--- sht31-controller/Support.hs
+++ sht31-controller/Support.hs
@@ -1,14 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Support (subscribe, persist) where
 
 import Data.Aeson
-import Control.Monad (void)
-import Data.Word (Word8)
+import Control.Monad (mzero, void)
+import Data.Word (Word8, Word32)
+import GHC.Generics
 import System.Environment (getEnv)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 import qualified Network.AMQP as AMQP
+
+data DataMessage = DataMessage
+    { busId :: Word32
+    , busAddr :: Word32
+    , message :: T.Text
+    }
+    deriving (Generic, Show)
+
+instance FromJSON DataMessage where
+    parseJSON (Object v) =
+        DataMessage <$>
+        v .: "busId" <*>
+        v .: "busAddr" <*>
+        v .: "data"
+    parseJSON _ = mzero
 
 e key = T.pack <$> getEnv key
 
@@ -54,6 +71,12 @@ handle respondFunc chan (AMQP.Message {AMQP.msgBody}, env@AMQP.Envelope {AMQP.en
 persist :: AMQP.Connection -> IO ()
 persist conn = getLine >> AMQP.closeConnection conn
 
+handleData :: (AMQP.Message, AMQP.Envelope) -> IO ()
+handleData (AMQP.Message {AMQP.msgBody}, env@AMQP.Envelope {AMQP.envRoutingKey}) = do
+    -- TODO: parse json, decode base64
+    AMQP.ackEnv env
+    return ()
+
 interface :: ToJSON a => T.Text -> a -> IO ()
 interface busAddress capabilities = do
     h <- getEnv "HOST"
@@ -63,6 +86,8 @@ interface busAddress capabilities = do
     conn <- AMQP.openConnection h "/" u p
     chan <- AMQP.openChannel conn
 
+    AMQP.declareExchange chan
+        AMQP.newExchange {AMQP.exchangeName, AMQP.exchangeType = "topic"}
 
     -- Initial capabilities advertisement
     let advertise = void $ AMQP.publishMsg chan exchangeName "global.conf" $ AMQP.newMsg {AMQP.msgBody = encode capabilities}
@@ -71,8 +96,11 @@ interface busAddress capabilities = do
     (globalReqQueueName, _, _) <- AMQP.declareQueue chan $
         AMQP.newQueue {AMQP.queueAutoDelete = True}
     AMQP.bindQueue chan globalReqQueueName exchangeName "global.req"
-    AMQP.consumeMsgs chan globalReqQueueName AMQP.Ack (const advertise)
+    AMQP.consumeMsgs chan globalReqQueueName AMQP.Ack $ \(_, env) -> advertise >> AMQP.ackEnv env
 
-
+    (dataQueueName, _, _) <- AMQP.declareQueue chan $
+        AMQP.newQueue {AMQP.queueAutoDelete = True}
+    AMQP.bindQueue chan dataQueueName exchangeName $ T.intercalate "." ["controller", busAddress, "#"]
+    AMQP.consumeMsgs chan dataQueueName AMQP.Ack handleData
 
     persist conn
