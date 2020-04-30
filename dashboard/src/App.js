08dca4f3e389976fadca00e6cf0a044080dabf0e
Emily Horsman <me@emilyhorsman.com>
2020-04-29 22:58:35 -0400

    Have basic dashboard display sensor data from WS

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [status, setStatus] = useState({});
+
+    useEffect(() => {
+        const socket = new WebSocket("wss://telemetry.0xt.ca/ws");
+        socket.onmessage = function(event) {
+            const data = JSON.parse(event.data);
+            setStatus({
+                ...status,
+                [data.uuid]: {data: data.data, timestamp: data.timestamp}
+            });
+        };
+    });
+
+    return (
+        <div className="App">
+            {Object.keys(status).map(uuid => {
+                const {temp, humidity, denominator} = status[uuid].data;
+                return (
+                    <p>
+                        {uuid}:
+                        {' '}
+                        {temp / denominator}°C,
+                        {' '}
+                        {humidity / denominator}% Humidity
+                    </p>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
