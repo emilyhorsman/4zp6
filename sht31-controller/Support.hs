24e9e183465ccbbe78ac145e895414acdb722041
Emily Horsman <me@emilyhorsman.com>
2020-04-29 23:55:09 -0400

    Remove superfluous logging in SHT31 processor

--- sht31-controller/Support.hs
+++ sht31-controller/Support.hs
@@ -39,47 +39,6 @@ instance FromJSON DataMessage where
 
 e key = T.pack <$> getEnv key
 
-{-
-type RespondFunc = T.Text -> [Word8] -> Maybe BL.ByteString
-
-subscribe :: [T.Text] -> RespondFunc -> IO AMQP.Connection
-subscribe busAddresses respondFunc = do
-    h <- getEnv "HOST"
-    u <- e "USER"
-    p <- e "PASS"
-    exchangeName <- e "EXCHANGE"
-    conn <- AMQP.openConnection h "/" u p
-    chan <- AMQP.openChannel conn
-
-    (queueName, _, _) <- AMQP.declareQueue chan
-        AMQP.newQueue {AMQP.queueAutoDelete = True}
-
-    AMQP.declareExchange chan
-        AMQP.newExchange {AMQP.exchangeName, AMQP.exchangeType = "topic"}
-
-    let controllerKeys = map (T.append "controller.") busAddresses
-    mapM_ (AMQP.bindQueue chan queueName exchangeName) controllerKeys
-    AMQP.bindQueue chan queueName exchangeName "global"
-
-    AMQP.consumeMsgs chan queueName AMQP.Ack $ handle respondFunc chan
-
-    return conn
-
-handle :: RespondFunc -> AMQP.Channel -> (AMQP.Message, AMQP.Envelope) -> IO ()
-handle respondFunc chan (AMQP.Message {AMQP.msgBody}, env@AMQP.Envelope {AMQP.envRoutingKey}) = do
-    exchangeName <- e "EXCHANGE"
-    let response = respondFunc envRoutingKey (BL.unpack msgBody)
-    print response
-    case response of
-        Nothing ->
-            return Nothing
-        Just res ->
-            AMQP.publishMsg chan exchangeName "backend"
-                AMQP.newMsg {AMQP.msgBody = res}
-
-    AMQP.ackEnv env
--}
-
 persist :: AMQP.Connection -> IO ()
 persist conn = getLine >> AMQP.closeConnection conn
 
@@ -101,12 +60,6 @@ handleData pub func (AMQP.Message {AMQP.msgBody}, env@AMQP.Envelope {AMQP.envRou
     case (decode msgBody :: Maybe DataMessage) of
         Nothing -> putStrLn "Failed to parse"
         Just result -> do
-            putStrLn "What"
-            print $ busId result
-            print $ busAddr result
-            print $ message result
-            print $ T.length $ message result
-            print $ de $ message result
             reply pub envRoutingKey (decodeUtf8 <$> BL.toStrict <$> encode <$> func (de (message result)))
 
 interface :: (ToJSON a, ToJSON b) => T.Text -> ([Word8] -> Maybe a) -> b -> IO ()
